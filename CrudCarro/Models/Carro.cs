c5b40f670cf508eceb49b5ec5895156769c0703e
Wilummp <lsousaah@gmail.com>
2024-03-18 12:48:21 -0300

    Adicionar arquivos de projeto.

---
+++ CrudCarro/Models/Carro.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudCarro.Models
+{
+    public class Carro
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Marca é obrigatório")]
+        public string Marca { get; set; }
+
+        [Required(ErrorMessage = "O campo Modelo é obrigatório")]
+        public string Modelo { get; set; }
+
+        [Required(ErrorMessage = "O campo Ano é obrigatório")]
+        public int Ano { get; set; }
+
+        [Required(ErrorMessage = "O campo Categoria é obrigatório")]
+        public string Categoria { get; set; }
+
+        [Required(ErrorMessage = "O campo Preço é obrigatório")]
+        public float Preco{ get; set; }
+
+    }
+}
